6bb2708db1f76591acc4d09abad7c1c638b7c1fc
jafidiesel <javier.bravin@osep.mendoza.gov.ar>
2018-10-08 13:00:45 -0300

    ADD: initial configuration babel 7 and webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Controller from './Controller.js';
+
+export default (function (window, document, containerName){
+    const controller = new Controller(window, document, containerName);
+})(window, document, 'container');
