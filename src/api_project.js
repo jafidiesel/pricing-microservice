155939348b9cc619b726e08c25d37faa6d1fdbcd
jafidiesel <javier.bravin@osep.mendoza.gov.ar>
2018-10-10 14:20:49 -0300

    ADD: Primera generación automática de apidoc

---
+++ src/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "currencyexchange-microservice",
+  "version": "1.0.0",
+  "description": "Currency exchange microservice",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-10-10T17:17:09.358Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
--- src/index.js
+++ src/index.js
@@ -10,23 +10,22 @@ console.log("Hello monkey!");
  * @api {get} /v1/currency/:monedaOrigen/:monedaDestino Convertir Monto
  * @apiName convertAmmount
  * @apiGroup Convert
+ * @apiPermission user
  *
  * @apiDescription Convierte un monto enviado de una moneda especificada a otra moneda especificada
  *
- * @apiParam {monto} Monto a convertir
+ * @apiParam {Number} Monto a convertir
  * 
  * @apiSuccessExample Success-Response:
- *    HTTP/1.1 200 Ok
- *  {
- *      "idMoneda": "{idMoneda}",  
- *      "codigoNumericoMoneda": "{codigoNumericoMoneda}",
- *      "montoConvertido": "{montoConvertido}",
- *      "abreviaturaMoneda": "{abreviaturaMoneda}",
- *      "simboloMoneda": "{simboloMoneda}"
- *  }
+ *      HTTP/1.1 200 Ok
+ *      {
+ *          "idMoneda": "{idMoneda}",  
+ *          "codigoNumericoMoneda": "{codigoNumericoMoneda}",
+ *          "montoConvertido": "{montoConvertido}",
+ *          "abreviaturaMoneda": "{abreviaturaMoneda}",
+ *          "simboloMoneda": "{simboloMoneda}"
+ *      }
  *
- * @apiUse ParamValidationErrors
- * @apiUse OtherErrors
  */
 export function convert(monto) {
     
