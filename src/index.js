54b437c946183a70980108ac79ea0cb6153bcff4
jafidiesel <javier.bravin@osep.mendoza.gov.ar>
2018-10-31 11:45:32 -0300

    ADD: json navigation

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@
 export default (function (window, document, containerName){
     const controller = new Controller(window, document, containerName);
 })(window, document, 'container'); */
-var jsonData;
 const app = document.getElementById('root');
 
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
-
 app.appendChild(container);
 
-console.log("Append succes");
 
 // Create a request variable and assign a new XMLHttpRequest object to it.
 var request = new XMLHttpRequest();
@@ -29,37 +26,11 @@ request.open('GET', apiUrl , true);
 request.onload = function () {
     // Begin accessing JSON data here
     var data = JSON.parse(this.response);
-    jsonData = JSON.parse(this.response);
-    console.log("--------");
-    debugger;
-    var query2 = data.results;
-    console.log(query2[query]);
+//    console.log(data.results[query].val);
 
     if( request.status >= 200 & request.status < 400 ){
-        console.log(data);
-        console.log(data.query);
-        /* data.forEach(results => {
-            console.log("Ingresa");
-            // Create a div with a card class
-            const card = document.createElement('div');
-            card.setAttribute('class', 'card');
-
-            // Create an h1 and set the text content to the film's title
-            /* const h1 = document.createElement('h1');
-            h1.textContent = currecy.results; */
-/*
-            // Create a p and set the text content to the film's description
-            const p = document.createElement('p');
-            p.textContent = `${results.ARS_USD.id}...`; // End with an ellipses
-
-            // Append the cards to the container element
-            container.appendChild(card);
-
-            // Each card will contain an h1 and a p
-            card.appendChild(h1);
-            card.appendChild(p);
-            
-        }); */
+        
+        return data.results;
     } else{
         const errorMessage = document.createElement('marquee');
         errorMessage.textContent = `Gah, it's not working!`;
