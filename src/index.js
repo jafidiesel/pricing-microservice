4900ce46291e3908f1ab7727df191ec63be6c55d
jafidiesel <jafibravin@outlook.com>
2018-11-12 19:55:10 -0300

    ADD: Create Currency Post

--- src/index.js
+++ src/index.js
@@ -7,29 +7,8 @@ export default (function (window, document, containerName){
 })(window, document, 'container'); 
 const app = document.getElementById('root');
 */
-var bodyParser = require('body-parser');
-var express = require('express');
-var app = express();
-
-app.use(bodyParser.json());
-
-app.get('/', function(req, res) {
-    res.send('raiz');
-});
-
-app.get('/v1/exchanger/currency/price', function(req, res) {
-    res.send('getQuote');
-});
-
-app.post('/testing', function(req, res) {
-    res.send('Llamada post');
-    console.log(req.body);
-});
 
 
-app.listen(3010, function() {
-    console.log('Escuchando en el puerto 3010');
-});
 
 /*
 const container = document.createElement('div');
---
+++ src/server/server.js
@@ -0,0 +1,61 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var mongoose = require('mongoose');
+var app = express();
+
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/currencyExchanger-db'); // <-- conexión
+
+//Modelo
+var currencySchema = new mongoose.Schema({
+    currencyNumericCode: { type: Number, required: true },
+    currencyDescription: { type: String, required: true },
+    currencyAbbreviation: { type: String, required: true },
+    currencySymbol: { type: String, required: true }
+});
+
+/*  mongoose.model registra el scheme
+    Currency es el nombre que tendra la coleccion de divisas a guardar
+    currencySchema sera el esquma que le correspondera
+*/
+var Currency = mongoose.model('Currency', currencySchema);
+
+// Alta currency
+app.post('/v1/exchanger/currency/', function(req, res) {
+    res.send('POST /v1/exchanger/currency/');
+    var currencyToCreate = new Currency();
+    
+    currencyToCreate.currencyNumericCode = req.body.currencyNumericCode;
+    currencyToCreate.currencyDescription = req.body.currencyDescription;
+    currencyToCreate.currencyAbbreviation = req.body.currencyAbbreviation;
+    currencyToCreate.currencySymbol = req.body.currencySymbol;
+
+    currencyToCreate.save( function(error, savedCurrency){
+        res.send(error);
+        //if (error) return res.status(500).send(error);
+        
+        res.status(201).json(savedCurrency);
+    } );
+
+});
+
+
+
+
+
+
+
+app.listen(3010, function() {
+    console.log('Escuchando en el puerto 3010');
+});
+
+app.get('/', function(req, res) {
+    res.send('raiz');
+});
+
+app.get('/v1/exchanger/currency/price', function(req, res) {
+    res.send('getQuote');
+});
+
+
