c95e3382eba670616b908dd15933742e61d0b5e0
jafidiesel <jafibravin@outlook.com>
2018-11-12 17:37:16 -0300

    ADD: configuracion inicial para poder escuchar desde la app utilizando express y body-parser

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-import { getQuote } from "./exchanger";
+//import getQuote from "./exchanger.js";
 
-import Controller from './Controller.js';
-
+//import Controller from './Controller.js';
+/*
 export default (function (window, document, containerName){
     const controller = new Controller(window, document, containerName);
 })(window, document, 'container'); 
 const app = document.getElementById('root');
+*/
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
 
+app.use(bodyParser.json());
 
+app.get('/', function(req, res) {
+    res.send('hola');
+});
+
+app.listen(3010, function() {
+    console.log('Escuchando en el puerto 3010');
+});
+
+/*
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 app.appendChild(container);
 
 
 console.log(getQuote('ARS', 'USD') );
+*/
