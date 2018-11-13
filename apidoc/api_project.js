0f3c0f8de4d664518b3a98ada4ff5a77d2fccb7e
jafidiesel <jafibravin@outlook.com>
2018-11-12 21:37:22 -0300

    MOVE: apidoc

---
+++ apidoc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "currencyexchange-microservice",
+  "version": "1.0.0",
+  "description": "Currency exchange microservice",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-10-23T15:10:37.044Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
