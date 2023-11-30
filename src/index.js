a634c93c2d2998beeab77d3fb00f5c828d0b1780
dev <jafibravin@outlook.com>
2023-11-30 13:02:43 -0300

    REBOOT

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Controller from './Controller.js';
-
-export default (function (window, document, containerName){
-    const controller = new Controller(window, document, containerName);
-})(window, document, 'container');
-
-console.log("Hello monkey!");
-
